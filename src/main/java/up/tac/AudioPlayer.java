8a4ef0f475d8de6b21e32b1d3a8b8213ae14085d
ali1x3 <avilaalex371@gmail.com>
2025-10-14 22:51:37 +0800

    allah

--- src/main/java/up/tac/AudioPlayer.java
+++ src/main/java/up/tac/AudioPlayer.java
@@ -1,6 +1,6 @@
 package up.tac;
 
-import java.applet.AudioClip;
+import java.awt.event.PaintEvent;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -8,84 +8,60 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import javax.print.attribute.standard.Media;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineEvent;
 
 public class AudioPlayer {
     record SoundData(AudioFormat format, byte[] pcm, int size) {}        
 
     private static AudioInputStream audioInputStream ;
-    private static ArrayList<Clip> clips = new ArrayList<>();
-    private static Clip clip;
+    private static ArrayList<Clip> playingClips = new ArrayList<>();
     private static Clip bgmClip;
-    private static HashMap<String, Clip> clipMap = new HashMap<>();
-    private static HashMap<String, SoundData> clipData = new HashMap<>();
+    private static SoundData data;
+    private static HashMap<String, SoundData> clipDataMap = new HashMap<>();
+    private static float bgmVolume = 0.5f;
+    private static float volume = 0.5f;
 
-    public static void play(String filePath, boolean isStoppable) {
+    public static void play(String filePath) {
         try{
-            clip = clipMap.computeIfAbsent(filePath, p -> {
+            data = clipDataMap.computeIfAbsent(filePath, p -> {
                 try {
                     URL url = AudioPlayer.class.getResource(p);
                     audioInputStream = AudioSystem.getAudioInputStream(loadStream(url.openStream()));
-                    clip = AudioSystem.getClip();
-                    //clip.open(audioInputStream);
                     AudioFormat format = audioInputStream.getFormat();
                     int size = (int) (format.getFrameSize() * audioInputStream.getFrameLength());
                     byte[] audio = new byte[size];
-                    DataLine.Info info = new DataLine.Info(Clip.class, format, size);
                     audioInputStream.read(audio, 0, size);
-
-                    clipData.put(filePath,
-                        new SoundData(format, audio, size)
-                    );
-                    return clip;
+                    
+                    return new SoundData(format, audio, size);
                 }
                 catch (Exception e) {
                     return null;
                 }
             });
-            if (true) {
-                System.out.println("the clip got rerun");
-                runClone(filePath);
-                return;
-            }
-            clip.setFramePosition(0);
-            clip.start();
+            Clip clip = AudioSystem.getClip();
+            clip.open(data.format, data.pcm(), 0, data.size());
 
-            if (isStoppable) {
-                clips.add(clip);
-            }
+            clip.setFramePosition(0);
+            
+            adjustVolume(clip, volume);
+            clip.start();
+            playingClips.add(clip);
+            clip.addLineListener(event -> {
+                if (event.getType() == LineEvent.Type.STOP) {
+                    clip.close();
+                    playingClips.remove(clip);
+                    System.out.println("clip removed");
+                }
+            });
 
         } catch (Exception ex) {
             System.out.println("Error with playing sound.");
             ex.printStackTrace();
-        }
-    }
-
-    private static void runClone(String filePath) {
-        try {
-            SoundData data = clipData.get(filePath);
-            Clip clip = AudioSystem.getClip();
-            clip.open(data.format, data.pcm(), 0, data.size());
-            
-            clip.setFramePosition(0);
-            clip.start();
-            System.out.println("clone running");
-            clip.addLineListener(event -> {
-            if (event.getType() == LineEvent.Type.STOP) {
-                clip.close();
-                System.out.println("clip closed");
-            }
-        });
-        }
-        catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -107,11 +83,14 @@ public class AudioPlayer {
         try{
             if(bgmClip != null && bgmClip.isRunning()) {
                 bgmClip.stop();
+                bgmClip.close();
             }
 
             audioInputStream = AudioSystem.getAudioInputStream(AudioPlayer.class.getResource(filePath));
             bgmClip = AudioSystem.getClip();
             bgmClip.open(audioInputStream);
+         
+            adjustVolume(bgmClip, bgmVolume);
             bgmClip.start();
             bgmClip.loop(Clip.LOOP_CONTINUOUSLY);
 
@@ -120,12 +99,45 @@ public class AudioPlayer {
             ex.printStackTrace();
         }
     }
-
-    public static void stop() {
-        for(Clip c : clips) {
+    public static void stopAll() {
+        for (Clip c : playingClips) {
             c.stop();
         }
-        clips.clear();
+        playingClips.clear();
     }
 
+    // these functions take a float between 0 and 1
+    // 0 being 0% and 1.0 being 100%
+    public static void setVolume(float newVal) {
+        volume = newVal;
+        volume = Math.max(0f, Math.min(volume, 1f));
+    }
+    
+    public static void setBgmVolume(float newVal) {
+        bgmVolume = newVal;
+        volume = Math.max(0f, Math.min(volume, 1f));
+    }
+
+    // DO not touch this only set the volume with the 2 methods above
+    private static void adjustVolume(Clip clip, float volume) {
+        if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+            float min = gainControl.getMinimum();
+            float max = gainControl.getMaximum();
+
+            float dB;
+            if (volume == 0f) {
+                dB = min;
+            } else {
+                dB = (float)(Math.log10(volume) * 20.0);
+                dB = Math.max(min, Math.min(dB, max));
+            }
+
+            gainControl.setValue(dB);
+            System.out.println(dB);
+        } else {
+            System.out.println("MASTER_GAIN not supported on this clip.");
+        }
+    }
 }
